181573635a8ebfc6a8b9388cb1c75ae35f70ac64
mrasked <asked.cross@mail.ru>
2020-10-05 00:35:08 +0300

    Last commit. Check bag's

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
         ArrayList<String> arrayList = new ArrayList<>();
         String line;
-        while ((line = reader.readLine()) != null) {
+        while ((line = reader.readLine())!=null) {
             arrayList.add(line);
             String[] array = arrayList.toArray(new String[arrayList.size()]);
             Arrays.sort(array);
@@ -19,4 +20,4 @@ public class Main {
             }
         }
     }
-}
+    }
