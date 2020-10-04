7f78c01df38dc9da13635275b60321dda7d20615
mrasked <asked.cross@mail.ru>
2020-10-04 23:40:32 +0300

    Добавил код сортировки массива

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FilterReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-        System.out.println("Sort Method");
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
+        ArrayList<String> arrayList = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            arrayList.add(line);
+            String[] array = arrayList.toArray(new String[arrayList.size()]);
+            Arrays.sort(array);
+            for (String s : array) {
+                System.out.println(" (" + s + ") ");
+            }
+        }
     }
 }
